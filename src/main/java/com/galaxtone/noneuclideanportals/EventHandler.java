724a6e52a08508381ed8c5e5af1ebebeee3a248b
Galaxtone <22679778+Galaxtone@users.noreply.github.com>
2018-05-22 06:14:55 -0400

    Removed un-needed loops and added some experimental render code

--- src/main/java/com/galaxtone/noneuclideanportals/EventHandler.java
+++ src/main/java/com/galaxtone/noneuclideanportals/EventHandler.java
@@ -1,12 +1,17 @@
 package com.galaxtone.noneuclideanportals;
 
-import com.galaxtone.noneuclideanportals.items.ItemBase;
+import com.galaxtone.noneuclideanportals.graphics.RenderHandler;
+import com.galaxtone.noneuclideanportals.items.ItemWand;
 import com.galaxtone.noneuclideanportals.utils.Selection;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -30,14 +35,14 @@ public class EventHandler {
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		IForgeRegistry<Item> registry = event.getRegistry();
-
-		for (ItemBase item : Register.itemList) registry.register(item);
+		
+		registry.register(ItemWand.instance);
 	}
 
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public static void registerModels(ModelRegistryEvent event) {
-		for (ItemBase item : Register.itemList) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(ItemWand.instance, 0, new ModelResourceLocation(ItemWand.instance.getRegistryName(), "inventory"));
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -45,8 +50,8 @@ public class EventHandler {
 	public static void renderWorld(RenderWorldLastEvent event) {
 		RenderHandler.update(event.getPartialTicks());
 		
-		ItemStack heldItem = Main.minecraft.thePlayer.getHeldItemMainhand();
-		boolean flag = heldItem != null && heldItem.getItem() == Register.wandItem;
+		ItemStack heldItem = Minecraft.getMinecraft().thePlayer.getHeldItemMainhand();
+		boolean flag = heldItem != null && heldItem.getItem() == ItemWand.instance;
 		
 		if (flag) Selection.updatePortalSelection();
 		RenderHandler.renderPortals(flag);
@@ -58,5 +63,18 @@ public class EventHandler {
 				RenderHandler.renderBlockSelection();
 			} else Selection.stop();
 		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public static void renderPlayer(RenderLivingEvent.Pre<AbstractClientPlayer> event) {
+		GlStateManager.pushMatrix();
+		GlStateManager.rotate(180F, 1F, 0F, 0F);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public static void renderPlayer(RenderLivingEvent.Post<AbstractClientPlayer> event) {
+		GlStateManager.popMatrix();
 	}
 }
