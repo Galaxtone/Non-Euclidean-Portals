c1c89d48d1101fed5cf2e021f5e53d45f5a0c89b
Galaxtone <22679778+Galaxtone@users.noreply.github.com>
2018-05-22 06:15:43 -0400

    Removved usage of decrepcated version of I18n

--- src/main/java/com/galaxtone/noneuclideanportals/graphics/gui/GuiConfig.java
+++ src/main/java/com/galaxtone/noneuclideanportals/graphics/gui/GuiConfig.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.galaxtone.noneuclideanportals.Main;
-import com.galaxtone.noneuclideanportals.utils.LocaleHelper;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.client.config.IConfigElement;
 public class GuiConfig extends net.minecraftforge.fml.client.config.GuiConfig {
 
 	public GuiConfig(GuiScreen parent) {
-		super(parent, getConfigElements(), Main.modid, false, false, LocaleHelper.translate(LocaleHelper.getKey("config", "title")));
+		super(parent, getConfigElements(), Main.modid, false, false, I18n.format("config." + Main.modid + ".title", Main.name));
 	}
 
 	private static List<IConfigElement> getConfigElements() {
@@ -24,7 +24,7 @@ public class GuiConfig extends net.minecraftforge.fml.client.config.GuiConfig {
 		Configuration configuration = Main.config.configuration;
 		for (String name : configuration.getCategoryNames()) {
 			ConfigCategory category = configuration.getCategory(name);
-			category.setLanguageKey(LocaleHelper.getKey("config", name, "title"));
+			category.setLanguageKey(String.format("config.%s.%s.title", Main.modid, name));
 			
 			if (name.indexOf(".") < 0) elements.add(new ConfigElement(category));
 		}
